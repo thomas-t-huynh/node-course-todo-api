e71121996c19faca25a6106ee98fe56056f76ce3
unknown <t_huynh927@yahoo.com>
2018-01-07 18:35:50 -0800

    heroku app upload

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODBURI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.export = {
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let express = require('express');
 let bodyParser = require('body-parser');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -47,8 +48,8 @@ app.get('/todos/:id', ( req , res ) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('started on port 3000');
+app.listen(port, () => {
+    console.log(`started on ${port}`);
 });
 
 
