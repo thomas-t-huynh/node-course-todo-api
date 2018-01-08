39193de577a7a1be4cd0e7268727a4bcbd4d5333
unknown <t_huynh927@yahoo.com>
2018-01-07 18:43:20 -0800

    fixed mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODBURI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.export = {
