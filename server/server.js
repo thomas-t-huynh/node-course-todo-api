f5c6858e063d8accfcbd905cb67e0d7ee1b832bd
unknown <t_huynh927@yahoo.com>
2018-01-07 19:46:44 -0800

    added a DELETE route, and learned deleting via queries

--- server/server.js
+++ server/server.js
@@ -48,6 +48,26 @@ app.get('/todos/:id', ( req , res ) => {
     });
 });
 
+app.delete('/todos/:id', ( req , res ) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(todo);
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
+
 app.listen(port, () => {
     console.log(`started on ${port}`);
 });
