210d5cfbd33df0dbed166ccd2c556e842905844d
unknown <t_huynh927@yahoo.com>
2018-01-17 02:27:20 -0800

    added middleware to hash and salt the users password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
     email: {
@@ -59,9 +60,10 @@ UserSchema.statics.findByToken = function (token) {
     try {
         decoded = jwt.verify(token, 'abc123');
     } catch (e) {
-        return new Promise((resolve , reject) => {
-            reject();
-        })
+        // return new Promise((resolve , reject) => {
+        //     reject();
+        // })
+        return Promise.reject();
     }
 
     return User.findOne({
@@ -69,7 +71,23 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.token': token,
         'tokens.access': 'auth'
     });
-}
+};
+
+UserSchema.pre('save', function (next) {
+   let user = this;
+   if (user.isModified('password')) {
+       
+        bcrypt.genSalt(10, (err , salt ) => {
+            bcrypt.hash(user.password , salt , (err , hash ) => {
+                user.password = hash;
+                next();
+            });
+        });
+
+   } else {
+       next();
+   }
+});
 
 let User = mongoose.model('User' , UserSchema );
 
