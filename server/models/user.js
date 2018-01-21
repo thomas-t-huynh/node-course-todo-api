721ba20e8efba203e7e5aaf2ff7b67d830e7a456
unknown <t_huynh927@yahoo.com>
2018-01-20 22:12:49 -0800

    Took a long ass time trying to create login authentication

--- server/models/user.js
+++ server/models/user.js
@@ -18,7 +18,7 @@ let UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        require: true,
+        required: true,
         minlength: 6
     },
     tokens: [{
@@ -70,6 +70,27 @@ UserSchema.statics.findByToken = function (token) {
         _id: decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
+    });
+};
+
+UserSchema.statics.findByCredentials = function (email , password) {
+    let User = this;
+
+    return User.findOne({email}).then((user) => {
+
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise ((resolve , reject) => {
+            bcrypt.compare( password , user.password , (err , res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
     });
 };
 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let { User } = require('./models/user');
 let { authenticate } = require('./middleware/authenticate');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 let app = express();
 
@@ -103,7 +104,7 @@ app.post('/users', (req, res) => {
     
     
     let body = _.pick(req.body, [ "email" , "password" ]);
-
+    
     let user = new User (body);
 
 
@@ -121,6 +122,21 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate , (req , res) => {
 
     res.send(req.user);
+
+});
+
+app.post('/users/login', (req , res) => {
+
+    let body = _.pick(req.body, [ "email" , "password" ]);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token ) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
 });
 
 app.listen(port, () => {
@@ -161,4 +177,3 @@ app.listen(port, () => {
 // })
 
 module.exports = {app};
-
