f91869ffbfebf3ecf3444140ac2cfb5b531d1ca8
unknown <t_huynh927@yahoo.com>
2018-01-28 11:50:39 -0800

    update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -44,7 +44,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     let user = this;
     let access = 'auth';
-    let token = jwt.sign({_id: user._id.toHexString(), access} , 'abc123');
+    let token = jwt.sign({_id: user._id.toHexString(), access} , process.env.JWT_SECRET);
 
     user.tokens = user.tokens.concat({ access , token });
 
@@ -68,7 +68,7 @@ UserSchema.statics.findByToken = function (token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         // return new Promise((resolve , reject) => {
         //     reject();
